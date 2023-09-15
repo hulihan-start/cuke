6e398ac10da0cdd763157694b993dfc72b8ef1ea
lihanhu <hulihan14@163.com>
2023-09-15 00:07:26 -0500

    safety checking update

--- lab/lab1.py
+++ lab/lab1.py
@@ -5,12 +5,13 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from core.ir import *
 from codegen.cpu import *
 
+
 def PrintCCode(ir):
-	code = ''
-	for d in ir:
-		if d:
-			code += to_string(d)
-	print(code)
+    code = ''
+    for d in ir:
+        if d:
+            code += to_string(d)
+    print(code)
 
 
 
@@ -47,9 +48,9 @@ def Loop0():
     lhs1 = Index(Index(Index(A, Expr(loopi.iterate, 1, '+')), loopj.iterate), loopk.iterate)
     lhs2 = Index(Index(Index(B, Expr(loopi.iterate, 1, '+')), Expr(loopj.iterate, 2, '+')), Expr(loopk.iterate, 1, '-'))
     rhs1 = Index(Index(Index(B, Expr(loopi.iterate, 1, '+')), loopj.iterate), Expr(loopk.iterate, 1, '-'))
-    rhs2 = Index(Index(Index(A, loopi.iterate), loopj.iterate),  Expr(loopk.iterate, 1, '+'))
-    rhs3 = Index(Index(Index(B, loopi.iterate), Expr(loopj.iterate, 2, '+')),  loopk.iterate)
-	
+    rhs2 = Index(Index(Index(A, loopi.iterate), loopj.iterate), Expr(loopk.iterate, 1, '+'))
+    rhs3 = Index(Index(Index(B, loopi.iterate), Expr(loopj.iterate, 2, '+')), loopk.iterate)
+
     # body = Assignment(lhs, Expr(rhs1, rhs2, '+'))
     loopk.body.extend([Assignment(lhs1, Expr(rhs1, 2, '+')), Assignment(lhs2, Expr(rhs2, rhs3, '+'))])
 
@@ -70,13 +71,13 @@ def Loop0():
 # 	}
 # }
 
-#Distance Vector: 
-#[1, 1, 0] :  a[i+1] [j+1] [k] and a [i] [j] [k]
-#[1, 0, -1] : a[i+1] [j+1] [k] and  a [i] [j + 1] [k + 1] 
+# Distance Vector:
+# [1, 1, 0] :  a[i+1] [j+1] [k] and a [i] [j] [k]
+# [1, 0, -1] : a[i+1] [j+1] [k] and  a [i] [j + 1] [k + 1]
 
-#Direction Vector:
-#[<, <, =]
-#[<, =, >]
+# Direction Vector:
+# [<, <, =]
+# [<, =, >]
 
 def Loop1():
     ir = []
@@ -94,8 +95,8 @@ def Loop1():
 
     lhs = Index(Index(Index(A, Expr(loopi.iterate, 1, '+')), Expr(loopj.iterate, 1, '+')), loopk.iterate)
     rhs1 = Index(Index(Index(A, loopi.iterate), loopj.iterate), loopk.iterate)
-    rhs2 = Index(Index(Index(A, loopi.iterate), Expr(loopj.iterate, 1, '+')),  Expr(loopk.iterate, 1, '+'))
-	
+    rhs2 = Index(Index(Index(A, loopi.iterate), Expr(loopj.iterate, 1, '+')), Expr(loopk.iterate, 1, '+'))
+
     body = Assignment(lhs, Expr(rhs1, rhs2, '+'))
     loopi.body.append(body)
 
@@ -114,11 +115,11 @@ def Loop1():
 # 	}
 # }
 
-#Distance Vector: 
-#[-1, 1]
+# Distance Vector:
+# [-1, 1]
 
-#Direction Vector:
-#[<, >]
+# Direction Vector:
+# [<, >]
 
 def Loop2():
     ir = []
@@ -133,7 +134,7 @@ def Loop2():
 
     lhs = Index(Index(A, loopi.iterate), loopj.iterate)
     rhs = Index(Index(A, Expr(loopi.iterate, 1, '+')), Expr(loopj.iterate, 1, '-'))
-	
+
     loopj.body.append(Assignment(lhs, rhs))
 
     ir.extend([Decl(N)])
@@ -142,56 +143,211 @@ def Loop2():
 
     return ir
 
-def InterchangeLoop(loop, loop_idx=[]):
-	#Implement here
-	pass
 
-def InterchangeLoopDemoOnSept122023(loop, loop_idx=[]):
+'''
+A[l0+1][l1][l2] = B[l0+1][l1][l2-1] + 2;
+B[l0+1][l1+2][l2-1] = A[l0][l1][l2+1] + B[l0][l1+2][l2];
+
+
+write statement array: [A[l0+1][l1][l2], B[l0+1][l1+2][l2-1]]
+read statement array: [B[l0+1][l1][l2-1], A[l0][l1][l2+1], B[l0][l1+2][l2]]
+
+Group index statement by names
+write dict: {'A': [A[l0+1][l1][l2]], 'B': [B[l0+1][l1+2][l2-1]]}
+read dict: {'A': [A[l0][l1][l2+1]], 'B': [B[l0+1][l1][l2-1], B[l0][l1+2][l2]]}
+
+write read & write write
+
+Compute the direction vector
+Distance vector
+A: [1, 0, -1]
+B1:[0, 2, 0]
+B2:[1, 0, -1]
+
+Direction vector
+A: [<, =, >]
+B1:[=, <, =]
+B2:[<, =, >]
+
+safety checking based on direction vector
+Exchange [0,1] first non-equal is '<', this is safe
+A: [=, <, >]
+B1:[<, =, =]
+B2:[=, <, >]
+
+Exchange [0,2]  not safe
+A: [>, =, <]    The first non-equal is '>', not valid.
+B1:[=, <, =]
+B2:[>, =, <]
+
+Exchange [1,2] 
+A: [<, >, =]
+B1:[=, =, <]
+B2:[<, >, =]
+'''
+
+
+def FindBody(loop):
+    if not isinstance(loop, Loop):
+        return loop
+    if isinstance(loop.body[0], Loop):
+        return FindBody(loop.body[0])
+    else:
+        return loop.body
+
+
+def get_index(statement, is_write, write_expr=[], read_expr=[]):
+    if isinstance(statement, Ndarray) or isinstance(statement, Index):
+        if is_write:
+            write_expr.append(statement)
+        else:
+            read_expr.append(statement)
+    elif isinstance(statement, Assignment):
+        get_index(statement.lhs, True, write_expr, read_expr)
+        get_index(statement.rhs, False, write_expr, read_expr)
+    elif isinstance(statement, Expr):
+        get_index(statement.left, is_write, write_expr, read_expr)
+        get_index(statement.right, is_write, write_expr, read_expr)
+    else:
+        return
+
+
+def get_array(wr_dict, stmt, parent_stmt):
+    if isinstance(stmt.dobject, Ndarray):
+        if stmt.dobject.name() in wr_dict.keys():
+            wr_dict[stmt.dobject.name()].append(parent_stmt)
+        else:
+            wr_dict[stmt.dobject.name()] = [parent_stmt]
+    elif isinstance(stmt.dobject, Index):
+        get_array(wr_dict, stmt.dobject, parent_stmt)
+
+
+def get_iterates(idx, vecs):
+    if isinstance(idx.dobject, Index):
+        # print(idx.index)
+        if isinstance(idx.index, Scalar):
+            if idx.index.val is None:
+                vecs.append(0)
+        elif isinstance(idx.index, Expr):
+            if idx.index.op == '+':
+                vecs.append(idx.index.right)
+            elif idx.index.op == '-':
+                vecs.append(-idx.index.right)
+        get_iterates(idx.dobject, vecs)
+    else:
+        if isinstance(idx.index, Scalar):
+            if idx.index.val is None:
+                vecs.append(0)
+        elif isinstance(idx.index, Expr):
+            if idx.index.op == '+':
+                vecs.append(idx.index.right)
+            elif idx.index.op == '-':
+                vecs.append(-idx.index.right)
+
+
+def direction_vec(write_idx, read_idx):
+    wvec = []
+    rvec = []
+    get_iterates(write_idx, wvec)
+    get_iterates(read_idx, rvec)
+    print(wvec, rvec)
+    return [x - y for x, y in zip(wvec, rvec)]
+
+
+def safety_checking(i0, i1, vecs):
+    tmp = []
+    for i in vecs:
+        subtmp = []
+        for j in i:
+            subtmp.append(j)
+        tmp.append(subtmp)
+    for i in tmp:
+        i[i0], i[i1] = i[i1], i[i0]
+
+    for i in tmp:
+        for j in i:
+            if j == 0:
+                continue
+            elif j > 0:
+                return True
+            elif j < 0:
+                return False
+    # print('after change: ', tmp)
+    # print('original vec:', vecs)
+
+
+def InterchangeLoop(ir, loop_idx=[]):
     ir_res = []
-    inner_most_loop_pointer = []
-    for item in loop:
-        if type(item)==Loop:
-            body = item.body[0]
-            while type(body) == Loop:
-                inner_most_loop_pointer = body
-                body = body.body[0]
-    
-    body = inner_most_loop_pointer.body
+    interchangeable = True
+    our_loop = None
+    write_expr = []
+    read_expr = []
+    write_dict = dict()
+    read_dict = dict()
+    for ir_item in ir:
+        if isinstance(ir_item, Loop):
+            body = FindBody(ir_item)
+            for body_item in body:
+                get_index(body_item, False, write_expr, read_expr)
 
+            PrintCCode(write_expr)
+            PrintCCode(read_expr)
+            print(write_expr, read_expr)
+            for i in write_expr:
+                get_array(write_dict, i, i)
+            for i in read_expr:
+                get_array(read_dict, i, i)
+            print(write_dict)
+            print(read_dict)
 
-    for item in body:
-        assert type(item)==Assignment
-        lhs = item.lhs
-        rhs = item.rhs
-        
-        idx = []
-        while type(lhs) == Index:
-            idx.append(lhs.index)
-            lhs = lhs.dobject
+            PrintCCode(write_dict['A'])
+            PrintCCode(write_dict['B'])
 
-        print(idx)
-        # if type(lhs) == Index:
-            
-        # else:
+            PrintCCode(read_dict['A'])
+            PrintCCode(read_dict['B'])
 
-        # if type(rhs) == Ndarray:
-    PrintCCode(inner_most_loop_pointer.body)
+            vecs = []
+            get_iterates(write_dict['A'][0], vecs)
+            get_iterates(write_dict['B'][0], vecs)
 
+            get_iterates(read_dict['A'][0], vecs)
+            get_iterates(read_dict['B'][0], vecs)
+            get_iterates(read_dict['B'][1], vecs)
+            print(vecs)
+            d_vec = []
+            for key in write_dict.keys():
+                if len(write_dict[key]) > 1:
+                    tmp = write_dict[key]
+                    for i in range(len(tmp)):
+                        for j in range(i + 1, len(tmp)):
+                            vec = direction_vec(tmp[i], tmp[j])
+                            d_vec.append(vec[::-1])
 
-    #print("Please implement the pass here")
-    pass
+                tmp = read_dict[key]
+                for i in tmp:
+                    vec = direction_vec(write_dict[key][0], i)
+                    d_vec.append(vec[::-1])
+            print(d_vec)
+
+            for i in range(len(d_vec[0])):
+                for j in range(i + 1, len(d_vec[0])):
+                    x = safety_checking(i, j, d_vec)
+                    print(x)
+
+    # print("Please implement the pass here")
+    return interchangeable, ir_res
+
 
 if __name__ == "__main__":
     loop0_ir = Loop0()
     loop1_ir = Loop1()
     loop2_ir = Loop2()
-    PrintCCode(loop0_ir)
+    # PrintCCode(loop0_ir)
 
-    optimized_loop1_ir = InterchangeLoop(loop0_ir, [0, 1])
-
-    #PrintCCode(optimized_loop1_ir)
-    # optimized_loop1_ir = InterchangeLoop(loop1_ir, [1, 2])
-    # optimized_loop2_ir = InterchangeLoop(loop2_ir, [0, 1])
+    optimized_loop0_ir, ir_res = InterchangeLoop(loop0_ir, [0, 1])
+    # PrintCCode(optimized_loop0_ir)
+    # optimized_loop1_ir = InterchangeLoop(loop1_ir, [1, 2]):
+    # optimized_loop2_ir = InterchangeLoop(loop2_ir, [0, 1]):
 
     # optimized_ir = LoopInterchange(ir)
     # print("Loop after interchange:")
